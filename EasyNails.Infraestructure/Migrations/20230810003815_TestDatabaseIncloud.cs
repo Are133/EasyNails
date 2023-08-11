87390ff4b026c4be879e842476973e0700b877c1
Are133 <isad9997@gmail.com>
2023-08-11 10:00:55 -0600

    Tests Deploys

---
+++ EasyNails.Infraestructure/Migrations/20230810003815_TestDatabaseIncloud.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyNails.Infraestructure.Migrations
+{
+    public partial class TestDatabaseIncloud : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "User");
+        }
+    }
+}
